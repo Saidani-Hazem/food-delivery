6461e612e89b28a80b3710f496fa4891d24b3aa0
Saidani Hazem <saidanihazem022@gmail.com>
2024-10-15 11:18:36 +0100

    DB connection

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express")
+const { default: mongoose } = require("mongoose")
 app = express()
 
 require("dotenv").config()
-
-
 app.listen(process.env.PORT , () => console.log(`Working on port ${process.env.PORT}`))
+
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected To DB")).catch()
